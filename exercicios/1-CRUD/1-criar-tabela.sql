8002a4153e3a065d9b2fc468e198799846c0d1c8
ymirella <yasmim.mirella.lima@g.globo>
2026-08-17 15:02:30 +0000

    adicionando exercicios 1 e 2

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,1 +1,6 @@
 -- Crie uma nova tabela no banco de dados
+CREATE TABLE salario_classe(
+id_salario PRIMARY KEY AUTOINCREMENT, -- Chave primária 
+salario REAL,
+nivel TEXT,
+);
--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,11 @@
 -- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
+ALTER TABLE salario_classe
+DROP nivel; -- No SQLite primeiro eu preciso excluir, depois alterar
+
+ALTER TABLE salario_classe
+ADD nivel VARCHAR(50);
+
 
 -- Na tabela employees, adicione uma coluna para receber o id_salario
+ALTER TABLE employees
+ADD id_salario INTEGER
